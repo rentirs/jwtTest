4ea29ad879f77806b672634dff2bd754874edd32
RAMavlikhanov <ren_tirs@mail.ru>
2023-02-12 11:09:04 +0300

    Initial commit

---
+++ src/main/java/com/rentirs/jwttest/user/Role.java
@@ -0,0 +1,6 @@
+package com.rentirs.jwttest.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/rentirs/jwttest/JwtTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentirs.jwttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
